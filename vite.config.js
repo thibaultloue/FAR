108522ba86e0f55890410fb4e6b8864c5c4b7abc
Thibault Loué <thibaultloue@MacBook-Air-de-Thibault.local>
2026-04-16 13:04:38 +0200

    ci: configure automatic GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const basePath =
+  process.env.GITHUB_ACTIONS === 'true' && repositoryName
+    ? `/${repositoryName}/`
+    : '/'
+
 export default defineConfig({
   plugins: [react()],
+  base: basePath,
 })
